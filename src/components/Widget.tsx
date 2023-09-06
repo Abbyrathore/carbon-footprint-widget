2a901ba4097a9a9970ef8ca68a873c869f1c735c
Abhishek Rathore <admin@Admins-MacBook-Pro-2.local>
2023-09-06 11:00:02 +0530

    initialization

---
+++ src/components/Widget.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export interface IWidgetProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    amount: number;
+    title?: string;
+}
+
+const Widget: React.FunctionComponent<IWidgetProps> = (props) => {
+     const { children, amount,title, style } = props;
+     let _style: React.CSSProperties = style || {};
+
+   
+
+  return (
+    <div style={_style} {...props}> {title || 'Widget'}{' '}{amount} {children}</div>
+  )
+}
+
+export default Widget
